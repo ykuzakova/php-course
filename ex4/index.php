6e709d0d0f3e271df6e944aa9b5c06019c85f128
yborzykina <yborzykina@plesk.com>
2022-03-15 19:25:16 +0700

    ex4 fix code review issues

--- ex4/index.php
+++ ex4/index.php
@@ -2,19 +2,18 @@
 /*Испытание 4: Реализуйте функцию addDigits(), которая принимает на вход неотрицательное целое число и возвращает другое число,
 полученное из первого следующим преобразованием: Итеративно сложите все входящие в него цифры до тех пор пока, не останется одна цифра.
 */
-    function addDigits($num)
-    {
-        $text = strval($num);
-        $sum = 0;
-        for($i = 0; $i < strlen($text); $i++) {
-            $sum = $sum + $text[$i];
-        }
-        $text = strval($sum);
-        if(strlen($text) === 1){
-            return $sum;
-        }
-
-        return addDigits($sum);
+function addDigits(int $num): int
+{
+    $text = (string)$num;
+    $sum = 0;
+    $len = strlen($text);
+    for ($i = 0; $i < $len; $i++) {
+        $sum = $sum + (int)$text[$i];
+    }
+    $text = (string)$sum;
+    if (strlen($text) === 1) {
+        return $sum;
     }
 
-
+    return addDigits($sum);
+}
